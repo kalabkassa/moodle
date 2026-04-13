57e30b0976b8a60f72d6338065fb494212aafae8
kalabkassa <kalabkassa@gmail.com>
2026-04-13 13:23:38 +0300

    on update section plugin update, not tested

--- local/lms/db/services.php
+++ local/lms/db/services.php
@@ -1,7 +1,7 @@
 $functions = [
     'local_lms_update_section' => [
         'classname'   => 'local_lms_external',
-        'methodname'  => 'update_section',
+        'methodname'  => 'sync_update_section',
         'classpath'   => 'local/lms/externallib.php',
         'description' => 'Update section name',
         'type'        => 'write',
